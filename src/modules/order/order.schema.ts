b8ff11cae449422e693d708be358ae625c128486
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2025-12-05 19:13:10 +0800

    refactor: change order status default value

--- src/modules/order/order.schema.ts
+++ src/modules/order/order.schema.ts
@@ -29,7 +29,7 @@ const orderSchema = new Schema({
                     "ready",
                     "delivered",
                 ],
-                default: "placed",
+                default: "confirmed",
             },
         },
     ],
